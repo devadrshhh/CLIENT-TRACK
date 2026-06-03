9ddf0c7990df7107de0178d88b3b94c95807be65
microxlearn <learn.microx@gmail.com>
2026-06-03 23:41:30 +0530

    new update

--- server/connection.js
+++ server/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-const directMongoURI = "mongodb://devadarshb01:Devadarshb01@cluster0-shard-00-00.pgqa5.mongodb.net:27017,cluster0-shard-00-01.pgqa5.mongodb.net:27017,cluster0-shard-00-02.pgqa5.mongodb.net:27017/clientTrackDB?ssl=true&replicaSet=atlas-p522v1-shard-0&authSource=admin&retryWrites=true&w=majority";
+const directMongoURI = "mongodb://devadarshb01:Devadarshb%4001@cluster0-shard-00-00.pgqa5.mongodb.net:27017,cluster0-shard-00-01.pgqa5.mongodb.net:27017,cluster0-shard-00-02.pgqa5.mongodb.net:27017/clientTrackDB?ssl=true&replicaSet=atlas-p522v1-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose.connect(directMongoURI)
-.then(() => console.log('MongoDB Connected!'))
+.then(() => console.log('Mongodb connected'))
 .catch((err) => console.log(err))
